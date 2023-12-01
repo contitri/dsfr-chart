a51ef9e242cd7203a2c9f9676e5a71af165aa819
Chouaib BRAKNI <chouaib.brakni@seenovate.com>
2023-12-01 11:02:05 +0100

    waffle-chart

--- src/components/WaffleChart.vue
+++ src/components/WaffleChart.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="container" id = "container">
+  <div class="container" id="container">
     <div v-for="(product, productIndex) in products" :key="productIndex" class="rectangle" :id="'div_' + product.name">
       <span class="x-label">{{ product.name }}</span>
     </div>
     <div class="graph">
-      <div v-for="(value, index) in this.zparse" :key="index" class="graph-item">
+      <div v-for="(value, index) in this.zparse" :key="index" class="graph-item" :style="{ flexBasis: getFlexBasis() }">
         <div :style="{ backgroundColor: getColor(index) }" class="graph-color"></div>
         <div class="graph-label">{{ value }}</div>
       </div>
@@ -14,13 +14,13 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       xparse: [],
       yparse: [],
       zparse: [],
       colors: ['#2f4077', '#447049', '#6e445a', '#FF33F6', '#F633FF']
-    }
+    };
   },
   props: {
     x: {
@@ -42,71 +42,66 @@ export default {
     color: {
       type: String,
       default: undefined
-    },
+    }
   },
   computed: {
-    products () {
+    products() {
       return this.xparse.map((name, index) => ({
         name,
         quantity: this.yparse[index]
-      }))
+      }));
     }
   },
   methods: {
-    resetData () {
-      this.xparse = []
-      this.yparse = []
-      this.zparse = []
+    resetData() {
+      this.xparse = [];
+      this.yparse = [];
+      this.zparse = [];
     },
-    adddiv () {
+    adddiv() {
       for (let i = 0; i < this.xparse.length; i++) {
-        const parentElement = document.getElementById("div_" + this.xparse[i])
+        const parentElement = document.getElementById("div_" + this.xparse[i]);
         for (let j = 0; j < this.yparse[i].length; j++) {
           for (let k = 0; k < this.yparse[i][j]; k++) {
-            const div = document.createElement('div')
-            div.id = 'square_' + this.xparse[i] + '_' + j + '_' + k
-            div.className = 'square'
-            div.style.backgroundColor = this.getColor(j)
-            div.style.width = '15px'
-            div.style.height = '15px'
-            div.style.display = 'inline-grid'
-            div.style.margin = '2px'
-            div.style.borderRadius = '15px'
-            div.style.verticalAlign = 'top'
-            div.style.direction = 'ltr'
-            parentElement.appendChild(div)
+            const div = document.createElement('div');
+            div.id = 'square_' + this.xparse[i] + '_' + j + '_' + k;
+            div.className = 'square';
+            div.style.backgroundColor = this.getColor(j);
+            div.style.width = '15px';
+            div.style.height = '15px';
+            div.style.display = 'inline-grid';
+            div.style.margin = '2px';
+            div.style.borderRadius = '15px';
+            div.style.verticalAlign = 'top';
+            div.style.direction = 'ltr';
+            parentElement.appendChild(div);
           }
         }
-        // const xLabel = document.createElement('span')
-        // xLabel.className = 'x-label'
-        // xLabel.textContent = this.xparse[i]
-        // xLabel.style.marginTop = '15px'
-        // parentElement.appendChild(xLabel)
-        // xLabel.style.transform = 'rotate(180deg)'
       }
     },
-    getColor (index) {
-      return this.colors[index % this.colors.length]
+    getColor(index) {
+      return this.colors[index % this.colors.length];
     },
-    getData () {
-      const self = this
-      this.xparse = JSON.parse(this.x)
-      this.yparse = JSON.parse(this.y)
-      this.zparse = JSON.parse(this.z)
+    getData() {
+      const self = this;
+      this.xparse = JSON.parse(this.x);
+      this.yparse = JSON.parse(this.y);
+      this.zparse = JSON.parse(this.z);
+    },
+    getFlexBasis() {
+      return this.zparse.length > 3 ? '33%' : '100%';
     }
   },
-
-  mounted () {
-    this.getData()
+  mounted() {
+    this.getData();
     this.$nextTick(() => {
-    this.adddiv()
-    })
-   }
-}
+      this.adddiv();
+    });
+  }
+};
 </script>
 
 <style scoped>
-
 .rectangle {
   display: inline-block;
   max-width: 100px;
@@ -115,6 +110,9 @@ export default {
   transform: rotate(180deg);
 }
 
+.container {
+  width: 500%;
+}
 .square {
   width: 15px;
   height: 15px;
@@ -129,22 +127,25 @@ export default {
 }
 
 .x-label {
-  font-size: 18px;
+  font-size: 10px;
   display: flex;
-  max-width: 100px;
+  width: 10px;
+  height: 10px;
   margin: 40px;
+  align-items: center;
 }
 
 .graph {
   display: flex;
-  align-items: center;
-  margin-top: 20px;
+  align-items: left;
+  margin-top: 30px;
+  flex-wrap: wrap;
+  max-width: 35%;
 }
 
 .graph-item {
   display: flex;
-  align-items: center;
-  margin-right: 10px;
+  align-items: left;
 }
 
 .graph-color {
@@ -156,11 +157,9 @@ export default {
 
 .graph-label {
   font-size: 16px;
-
 }
 
-.x-label{
-  transform: rotate(-180deg);
+.x-label {
+  transform: rotate(-225deg);
 }
-
 </style>
